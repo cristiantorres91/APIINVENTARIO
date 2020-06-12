a04f8ae7b156b4d64a86c50e11ae35ab43e8b4da
cristiantorres91 <49564966+cristiantorres91@users.noreply.github.com>
2020-06-11 20:52:03 -0600

    API
    API INVENTARIO

---
+++ WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        inventarioEntities db = new inventarioEntities();
+
+        [HttpGet]
+        public IHttpActionResult Login(string user, string pass)
+        {
+
+            using (inventarioEntities db = new inventarioEntities())
+            {
+                var login = db.usuarios.Where(x => x.usuario == user && x.clave == pass).FirstOrDefault();
+                if (login != null)
+                    return Ok(new usuarios() {nombre_completo = login.nombre_completo, id_rol = login.id_rol, id_usuario = login.id_usuario, usuario = login.usuario, clave = login.clave, dui = login.dui, estado = login.estado, estado_sesion = login.estado_sesion, fecha_ultimo_ingreso = login.fecha_ultimo_ingreso, hora_ultimo_ingreso = login.hora_ultimo_ingreso } );
+                else
+                    return NotFound();
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/ProductosController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Expiration(string dateInit, string dateEnd)
+        {
+            DateTime dateI = Convert.ToDateTime(dateInit);
+            DateTime dateF = Convert.ToDateTime(dateEnd);
+            using (inventarioEntities db = new inventarioEntities())
+            {
+
+                var product = (from p in db.productos
+                              join m in db.detalles_movimientos on p.id_producto equals m.id_producto
+                              where m.fecha_vencimiento >= dateI && m.fecha_vencimiento <= dateF
+                              select new { Producto = p.nombre }).ToList();
+
+                if (product != null)
+                    return Ok(product);
+                else
+                    return NotFound();
+
+            }
+        }
+    }
+}
